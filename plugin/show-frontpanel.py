15a72fdff54e24d71befac72270b4232e6805128
Roman <dodin.roman@gmail.com>
2026-03-03 00:04:16 +0100

    cli env wiring

--- plugin/show-frontpanel.py
+++ plugin/show-frontpanel.py
@@ -1,11 +1,15 @@
+import argparse
 import json
 import os
+import shutil
 import subprocess
 import sys
-import shutil
 
 from srlinux.location import build_path
-from srlinux.mgmt.cli import CliPlugin
+from srlinux.mgmt.cli import CliPlugin, CommandNodeWithArguments, RequiredPlugin
+from srlinux.mgmt.cli.cli_loader import CliLoader
+from srlinux.mgmt.cli.cli_output import CliOutput
+from srlinux.mgmt.cli.cli_state import CliState
 from srlinux.syntax import Syntax
 
 
@@ -62,10 +66,10 @@ class Plugin(CliPlugin):
             RequiredPlugin(module="srlinux", plugin="platform_reports"),
         ]
 
-    def load(self, cli, **_kwargs):
+    def load(self, cli: CliLoader, arguments: argparse.Namespace):
         platform = cli.show_mode.root.get_command("platform")
 
-        frontpanel = platform.add_command(
+        platform.add_command(
             syntax=self._syntax(),
             callback=self._print,
         )
@@ -77,7 +81,13 @@ class Plugin(CliPlugin):
             help="Show image of front panel",
         )
 
-    def _print(self, state, arguments, output, **_kwargs):
+    def _print(
+        self,
+        state: CliState,
+        output: CliOutput,
+        arguments: CommandNodeWithArguments,
+        **_kwargs,
+    ):
         chassis_path = build_path("/platform/chassis/type")
         chassis_server_data = state.server_data_store.get_data(
             chassis_path, recursive=False
@@ -115,11 +125,6 @@ class Plugin(CliPlugin):
                 f"Failed to render front panel image (protocol={protocol}): {proc.stderr.strip()}"
             )
 
-        sys.stdout.flush()
-
-        front_panel_path = build_path("/platform/front-panel")
-        front_panel_server_data = state.server_data_store.get_data(
-            front_panel_path, recursive=False
+        output.print(
+            f"\n\n⚡ High resolution image: https://go.srlinux.dev/img-{chassis.type.replace(' ', '-').lower()}\n"
         )
-        front_panel = front_panel_server_data.platform.get().front_panel.get()
-        output.print(f"\n\nHigh resolution image: {front_panel.url}\n")
